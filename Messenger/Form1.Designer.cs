53a09265d098bf6fd4903618496bae46d03b3c73
Eric Buchanan <42704734+EricMBu@users.noreply.github.com>
2022-03-26 15:52:06 -0400

    Add files via upload

---
+++ Messenger/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Messenger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatWindow = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.ScreenNameBox = new System.Windows.Forms.TextBox();
+            this.btnNameset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IPaddress = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.portNumber = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.changePort = new System.Windows.Forms.LinkLabel();
+            this.connectionStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chatWindow
+            // 
+            this.chatWindow.Location = new System.Drawing.Point(57, 34);
+            this.chatWindow.Name = "chatWindow";
+            this.chatWindow.ReadOnly = true;
+            this.chatWindow.Size = new System.Drawing.Size(258, 295);
+            this.chatWindow.TabIndex = 0;
+            this.chatWindow.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 338);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(321, 348);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(60, 28);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(445, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Peer IP";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(706, 34);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(60, 20);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(277, 108);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(114, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.Text = "Screen Name Only";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(277, 131);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "IP Only";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(277, 154);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(124, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Screen Name and IP";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // ScreenNameBox
+            // 
+            this.ScreenNameBox.Location = new System.Drawing.Point(99, 27);
+            this.ScreenNameBox.Name = "ScreenNameBox";
+            this.ScreenNameBox.Size = new System.Drawing.Size(255, 20);
+            this.ScreenNameBox.TabIndex = 9;
+            // 
+            // btnNameset
+            // 
+            this.btnNameset.Location = new System.Drawing.Point(360, 27);
+            this.btnNameset.Name = "btnNameset";
+            this.btnNameset.Size = new System.Drawing.Size(60, 20);
+            this.btnNameset.TabIndex = 10;
+            this.btnNameset.Text = "Set";
+            this.btnNameset.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Screen name";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.changePort);
+            this.groupBox1.Controls.Add(this.portNumber);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.IPaddress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnNameset);
+            this.groupBox1.Controls.Add(this.ScreenNameBox);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(351, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 209);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client settings";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Display:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "IP:";
+            // 
+            // IPaddress
+            // 
+            this.IPaddress.AutoSize = true;
+            this.IPaddress.Location = new System.Drawing.Point(47, 81);
+            this.IPaddress.Name = "IPaddress";
+            this.IPaddress.Size = new System.Drawing.Size(35, 13);
+            this.IPaddress.TabIndex = 14;
+            this.IPaddress.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Port:";
+            // 
+            // portNumber
+            // 
+            this.portNumber.AutoSize = true;
+            this.portNumber.Location = new System.Drawing.Point(47, 112);
+            this.portNumber.Name = "portNumber";
+            this.portNumber.Size = new System.Drawing.Size(37, 13);
+            this.portNumber.TabIndex = 16;
+            this.portNumber.Text = "49200";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(711, 409);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(77, 29);
+            this.btnDisconnect.TabIndex = 13;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // changePort
+            // 
+            this.changePort.AutoSize = true;
+            this.changePort.Location = new System.Drawing.Point(96, 112);
+            this.changePort.Name = "changePort";
+            this.changePort.Size = new System.Drawing.Size(43, 13);
+            this.changePort.TabIndex = 18;
+            this.changePort.TabStop = true;
+            this.changePort.Text = "change";
+            this.changePort.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.changePort_LinkClicked);
+            // 
+            // connectionStatus
+            // 
+            this.connectionStatus.AutoSize = true;
+            this.connectionStatus.Location = new System.Drawing.Point(555, 57);
+            this.connectionStatus.Name = "connectionStatus";
+            this.connectionStatus.Size = new System.Drawing.Size(35, 13);
+            this.connectionStatus.TabIndex = 14;
+            this.connectionStatus.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connectionStatus);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chatWindow);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Eric\'s Easy Messenger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox chatWindow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox ScreenNameBox;
+        private System.Windows.Forms.Button btnNameset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label portNumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label IPaddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.LinkLabel changePort;
+        private System.Windows.Forms.Label connectionStatus;
+    }
+}
+
